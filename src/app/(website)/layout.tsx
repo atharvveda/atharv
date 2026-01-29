312f9eebf165aab0ffe065603f9dc73813a3620f
Ankur0191 <ankurmishra11187@gmail.com>
2026-01-30 02:43:37 +0530

    layout ahref updated

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -135,15 +135,23 @@ export default function RootLayout({
         {/* Google Tag Manager */}
         <Script id="gtm-script" strategy="beforeInteractive">
           {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-TD492XD4');
-          `}
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-TD492XD4');
+    `}
         </Script>
         {/* End Google Tag Manager */}
+
+        {/* Ahrefs Web Analytics */}
+        <Script
+          src="https://analytics.ahrefs.com/analytics.js"
+          data-key="SIKdrSujSdMovr9jcFfYIw"
+          strategy="afterInteractive"
+        />
       </head>
+
 
       <body className={`${archivo.variable} ${inter.variable}`}>
         {/* Google Tag Manager (noscript) */}
